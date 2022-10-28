210a7f195f67a262f4d8536ca4fa7087b4233aa4
hasetaku21 <takumi_hase21@outlook.jp>
2022-10-28 20:44:49 +0900

    Initial Commit

---
+++ buttonTappedApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  buttonTappedApp
+//
+//  Created by a on 2022/10/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
